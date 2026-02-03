de72aa5d0234ac61233e7cc39edf831a83070bb6
joseMigueel <josemiguemarre99@gmail.com>
2026-02-03 19:04:50 +0000

    Añadimos datos de propiedad y map()

---
+++ vite-project/src/data/properties.js
@@ -0,0 +1,22 @@
+export const properties = [{
+    id : 1,
+    title: "Apartamento en la playa",
+    location: "Mogan",
+    price: 90,
+    image: "https://via.placeholder.com/400x250"
+},
+{
+    id : 2,
+    title: "Villa con piscina",
+    location: "Tejeda",
+    price: 90,
+    image: "https://via.placeholder.com/400x250"
+},
+{
+    id : 3,
+    title: "Apartamento rústico",
+    location: "Las Palmas",
+    price: 90,
+    image: "https://via.placeholder.com/400x250"
+}
+]
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import Header from "../components/Header"
 import SearchBar from "../components/SearchBar"
 import PropertyCard from "../components/Propertycard";
+import { properties } from "../data/properties";
+
 function Home () {
     return (
         <>
         <Header />
         <SearchBar/>
         <h2>Alojamientos disponibles</h2>
-        <PropertyCard
-        title= "Apartamento en la playa"
-        location= "Mogan"
-        price={90}
-        image="https://via.placeholder.com/400x250"/>        
+        {properties.map(properties => (
+            <PropertyCard
+                key={properties.id}
+                title={properties.title}
+                location={properties.location}
+                price={properties.price}
+                image={properties.image}/>  
+        ))}
         </>
     )
 }
